34875dd73172519dd14dc7c46a67832ef7fcfc05
quinnciples <49799428+quinnciples@users.noreply.github.com>
2020-12-01 21:00:59 -0700

    More log timing issues resolved.

--- SixtyFiveOhTwo.py
+++ SixtyFiveOhTwo.py
@@ -50,15 +50,16 @@ class CPU6502:
         self.memory = [0] * CPU6502.MAX_MEMORY_SIZE
 
     def readMemory(self):
+        self.logState()
         data = self.memory[self.program_counter]
         self.program_counter += 1
         self.cycles += 1
-        self.logState()
+        
         return data
 
     def execute(self):
         while self.cycles < self.cycle_limit:
-            self.logState()
+            # self.logState()
             opcode = self.readMemory()
             if CPU6502.opcodes[opcode] == 'LDA_IM':
                 # Load memory into accumulator
@@ -74,21 +75,22 @@ class CPU6502:
                     self.registers['N'] = 1
                 else:
                     self.registers['N'] = 0
+            self.logState()
 
     def printState(self):
-        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
+        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
         headerString = '\t'.join(combined)
         valueString = '\t'.join(str(v) for v in combined.values())
         print(headerString)
         print(valueString)
 
     def initializeLog(self):
-        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
+        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
         headerString = '\t'.join(combined)
         self.log.append(headerString)
 
     def logState(self):
-        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
+        combined = {**{'Cycle': self.cycles}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 4)}}
         valueString = '\t'.join(str(v) for v in combined.values())
         self.log.append(valueString)
 
