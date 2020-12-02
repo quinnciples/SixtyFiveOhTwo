3cf5f9a502d0b067a272e5afb87bf9b599051657
quinnciples <49799428+quinnciples@users.noreply.github.com>
2020-12-01 19:58:04 -0700

    Added printState function.

--- SixtyFiveOhTwo.py
+++ SixtyFiveOhTwo.py
@@ -1,9 +1,15 @@
 # 6502 machine code processor
+from enum import Enum
+
+
+class OPCODE(Enum):
+    LDA_IM = 0xA9
 
 
 class CPU6502:
 
     version = '0.01'
+    MAX_MEMORY_SIZE = 1024 * 64  # 64k memory size
 
     def __init__(self):
 
@@ -26,6 +32,10 @@ class CPU6502:
             'N': 0
         }
 
+        self.memory = [0] * CPU6502.MAX_MEMORY_SIZE
+
+        self.cycles = 0
+
     def reset(self):
         self.program_counter = 0xFFFC
         self.stack_pointer = 0x0100
@@ -36,8 +46,10 @@ class CPU6502:
         # Reset all flags to zero
         self.flags = dict.fromkeys(self.flags.keys(), 0)
 
+        self.memory = [0] * CPU6502.MAX_MEMORY_SIZE
+
     def printState(self):
-        combined = {**self.registers, **self.flags}
+        combined = {**self.registers, **self.flags, **{'PC': '0x{0:0{1}X}'.format(self.program_counter,4), 'SP': '0x{0:0{1}X}'.format(self.stack_pointer,4)}}
         headerString = '\t'.join(combined)
         valueString = '\t'.join(str(v) for v in combined.values())
         print(headerString)
