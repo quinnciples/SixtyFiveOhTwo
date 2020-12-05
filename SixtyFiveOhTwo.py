1af927726700ae16f637c2ab3dd4ef9ed9a6452f
quinnciples <49799428+quinnciples@users.noreply.github.com>
2020-12-05 14:06:54 -0700

    Updated program code and test code

--- SixtyFiveOhTwo.py
+++ SixtyFiveOhTwo.py
@@ -55,7 +55,10 @@ class CPU6502:
         self.registers = {
             'A': 0,
             'X': 0,
-            'Y': 0,
+            'Y': 0
+        }
+
+        self.flags = {
             'C': 0,
             'Z': 0,
             'I': 0,
@@ -97,6 +100,9 @@ class CPU6502:
         # Reset all registers to zero
         self.registers = dict.fromkeys(self.registers.keys(), 0)
 
+        # Reset all flags to zero
+        self.flags = dict.fromkeys(self.flags.keys(), 0)
+
         self.memory = [0] * CPU6502.MAX_MEMORY_SIZE
 
     def readMemory(self, increment_pc=True, address=None) -> int:
@@ -114,14 +120,14 @@ class CPU6502:
     def setFlags(self, register, flags=[]):
         if 'Z' in flags:
             if self.registers[register] == 0:
-                self.registers['Z'] = 1
+                self.flags['Z'] = 1
             else:
-                self.registers['Z'] = 0
+                self.flags['Z'] = 0
         if 'N' in flags:
             if self.registers[register] & 0b10000000 > 0:
-                self.registers['N'] = 1
+                self.flags['N'] = 1
             else:
-                self.registers['N'] = 0
+                self.flags['N'] = 0
 
     def execute(self):
         data = self.readMemory()
@@ -210,19 +216,19 @@ class CPU6502:
             self.INS = CPU6502.opcodes.get(data, None)
 
     def printState(self):
-        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
+        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
         headerString = '\t'.join(combined)
         valueString = '\t'.join(str(v) for v in combined.values())
         print(headerString)
         print(valueString)
 
     def initializeLog(self):
-        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
+        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
         headerString = '\t'.join(combined)
         self.log.append(headerString)
 
     def logState(self):
-        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
+        combined = {**{'Cycle': self.cycles, '%-10s' % 'INS': '%-10s' % self.INS}, **self.registers, **self.flags, **{'SP': '0x{0:0{1}X}'.format(self.stack_pointer, 4), 'PC': '0x{0:0{1}X}'.format(self.program_counter, 4), 'MEM': '0x{0:0{1}X}'.format(self.memory[self.program_counter], 2)}}
         valueString = '\t'.join(str(v) for v in combined.values())
         self.log.append(valueString)
 
