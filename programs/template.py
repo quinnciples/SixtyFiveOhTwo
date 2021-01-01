cc2c54feeecc0756550c72a2cf9371d91f95b783
quinnciples <49799428+quinnciples@users.noreply.github.com>
2021-01-01 12:22:48 -0700

    Moved wozmon and hello world to modules.

---
+++ programs/template.py
@@ -0,0 +1,21 @@
+name = ""
+
+description = """
+
+"""
+
+asm_source = """
+
+"""
+
+program = [
+]
+
+starting_address = 0x8000
+
+tests = [
+    {
+        'memory_range': [Start, Finish],
+        'expected_values': [Value1, Value2, Value3]
+    }
+]
